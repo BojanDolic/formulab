430b5eb399679199bd84b673daf0914382755271
BojanDolic <dolicbojan2@gmail.com>
2022-05-22 00:03:33 +0200

    ABOUT_US_SCREEN
    Implement navigation component.
    Add about us screen.

---
+++ app/src/main/java/com/aggressiveclick/formulab/ui/fragments/MainFragment.kt
@@ -0,0 +1,42 @@
+package com.aggressiveclick.formulab.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.aggressiveclick.formulab.databinding.MainFragmentBinding
+import com.aggressiveclick.formulab.ui.viewmodels.MainViewModel
+
+class MainFragment : Fragment() {
+
+    private val viewModel by viewModels<MainViewModel>()
+
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding.aboutUsBtn.setOnClickListener {
+            findNavController().navigate(MainFragmentDirections.actionMainFragmentToAboutFragment())
+        }
+
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
